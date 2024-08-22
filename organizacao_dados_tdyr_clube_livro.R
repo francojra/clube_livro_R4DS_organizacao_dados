392083b2566dc7dd392f277ec4c5031e14ee564b
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2024-08-22 19:01:37 -0300

    Add files via upload

---
+++ organizacao_dados_tdyr_clube_livro.R
@@ -0,0 +1,26 @@
+
+# Clube do Livro R for Data Science -------------------------------------------------------------------------------------------------------------
+# Encontro 8: Organização de dados (pacote tidyr) -----------------------------------------------------------------------------------------------
+# Data: 06/08/2024 ------------------------------------------------------------------------------------------------------------------------------
+
+# Conceitos --------------------------------------------------------------------------------------------------------------------------------
+
+## Existem três regras que fazem com que um conjunto de dados seja
+## seja considerado tidy:
+
+## - Cada variável é uma coluna
+## - Cada observação é uma linha
+## - Cada valor é uma célula; cada célula um único valor
+
+## Os pacotes dplyr, ggplot2 e os demais pacotes do tidyverse
+## foram pensados para trabalhar com dados tidy.
+
+# Instalar pacotes ------------------------------------------------------------------------------------------------------------------------------
+
+##install.packages("tidyverse")
+##install.packages("dados") # Conjunto de dados traduzidos
+
+# Carregar pacotes ------------------------------------------------------------------------------------------------------------------------------
+
+library(tidyverse)
+library(dados)
